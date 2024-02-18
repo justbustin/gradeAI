9ab0a857d6da6430a07dfe2ee7ff5973e76ce7c7
Kenny Wan <kennethzwan@gmail.com>
2024-02-17 23:53:34 -0800

    upload finished and home page styled

---
+++ gradeAI/pages/assignment.py
@@ -0,0 +1,19 @@
+from gradeAI.templates import template
+import os
+from pathlib import Path
+from typing import List
+
+import reflex as rx
+
+
+@template(route="/assignment", title="Assignment")
+def assignment() -> rx.Component:
+    return rx.vstack(
+        rx.text("Assignments"),
+        rx.hstack(
+            rx.text("Name"),
+            rx.text("Status"),
+            rx.text("Date Posted"),
+        ),
+        rx.text("Exams"),
+    )
